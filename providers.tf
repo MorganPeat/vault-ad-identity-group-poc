0faf500e71c35a7b91f784876df635348a529763
Morgan Peat <MorganPeat@users.noreply.github.com>
2023-11-29 15:18:27 +0000

    Create a test AAD group, users, and assign users into the group

--- providers.tf
+++ providers.tf
@@ -1,10 +1,12 @@
+# Azure authN is by workload identity using env variables
+# stored in this workspace
+# See https://registry.terraform.io/providers/hashicorp/azuread/latest/docs/guides/service_principal_client_secret#environment-variables
+provider "azuread" {}
+
+provider "random" {}
+
 # Enables access to secrets stored in vault.
 # Vault authN is by workload identity using env variables
 # stored in this workspace.
 # See https://developer.hashicorp.com/terraform/cloud-docs/workspaces/dynamic-provider-credentials/vault-configuration.
 provider "vault" {}
-
-# Azure authN is by workload identity using env variables
-# stored in this workspace
-# See https://registry.terraform.io/providers/hashicorp/azuread/latest/docs/guides/service_principal_client_secret#environment-variables
-provider "azuread" {}
--- versions.tf
+++ versions.tf
@@ -7,6 +7,10 @@ terraform {
       source  = "hashicorp/azuread"
       version = "~> 2.15.0"
     }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.5.1"
+    }
     vault = {
       source  = "hashicorp/vault"
       version = "~> 3.15.0"
