c759b6e2929f8dda183fb3b64fa82b944c275be7
Morgan Peat <MorganPeat@users.noreply.github.com>
2023-11-30 14:51:56 +0000

    OIDC authN to AzureAD works. Policies are applied at the AD Group level

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "vault_address" {
+  type        = string
+  description = "Public URL of the HCP Vault Cluster"
+}
+
+variable "azure_tenant_id" {
+  type        = string
+  description = "Tenant ID of Azure AD"
+}
